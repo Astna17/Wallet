76200112f50392e0332ed7c10f500a9859db53df
Astna17 <hei.antsa.17@gmail.com>
2023-12-08 13:40:59 +0300

    feat: add class TransferHistory in package Entity

---
+++ src/main/java/com/functionality/td_wallet/entity/TransferHistory.java
@@ -0,0 +1,10 @@
+package org.bibliotheque.Entity;
+
+import java.time.LocalDateTime;
+
+public class TransferHistory {
+    private int idTransfer;
+
+    public TransferHistory(int debitTransactionId, int creditTransactionId, LocalDateTime transferDate) {
+    }
+}
