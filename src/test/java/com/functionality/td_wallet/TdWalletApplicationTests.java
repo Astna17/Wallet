019f4820b036ca59fff192666f553da4c0b5a20f
Astna17 <hei.antsa.17@gmail.com>
2023-12-08 12:46:26 +0300

    first commit

---
+++ src/test/java/com/functionality/td_wallet/TdWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.functionality.td_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
